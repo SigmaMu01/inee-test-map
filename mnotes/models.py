fefddbc3577e185945825f1fc53fe2bd65e4ea30
SigmaMu01 <egor.maksimov.d@gmail.com>
2024-05-16 20:54:17 +0500

    Clean up models.py and refurbish main database structure

--- mnotes/models.py
+++ mnotes/models.py
@@ -1,10 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.contrib.gis.db import models
 
-from mnotes.app_defaults import *
-
 
 class User(AbstractUser):
+    """Extended user table with an assigned list of notes"""
     map_pins = models.ManyToManyField("UserMapNote", blank=True)
 
 
@@ -13,7 +12,7 @@ class UserMapNote(models.Model):
     title = models.CharField(max_length=64, verbose_name="Title")
     description = models.TextField(verbose_name="Description")
     date_published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name="Published")
-    map_pin_point = models.PointField(default="0101000020E61000000700004002806F3FEC03C83F02E069BF", verbose_name="Coordinates")
+    map_pin_point = models.PointField(verbose_name="Coordinates")
 
     class Meta:
         verbose_name = "Note"
