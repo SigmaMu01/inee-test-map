614c8adbabb99ee51afafafc35f4cb567bedf9ee
SigmaMu01 <egor.maksimov.d@gmail.com>
2024-05-17 04:36:51 +0500

    Modify settings.py to support new .env names

--- mapawish/settings.py
+++ mapawish/settings.py
@@ -90,11 +90,11 @@ load_dotenv()  # Load from file .env
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
 
     }
 }
